54368e5723f13db933ec6beb2ccff5e7171b4fb1
Cameron Tetford <cameron.tetford+github@gmail.com>
2023-07-18 13:53:37 -0400

    Update OpenAstroDome.h

--- SharedLibs/Configuration/OpenAstroDome.h
+++ SharedLibs/Configuration/OpenAstroDome.h
@@ -15,7 +15,7 @@ constexpr int32_t MinStepPosition = -2000000000L;
 #define ROTATOR_MOTOR_TYPE  (DC_MOTOR) // See Constants.h for options
 #define SHUTTER_MOTOR_TYPE  (DC_MOTOR) // See Constants.h for options
 // Motor Parameters
-#define MICROSTEPS_PER_STEP (2) // Should match DIP switches on stepper driver
+#define MICROSTEPS_PER_STEP (8) // Should match DIP switches on stepper driver
 #define MIN_SPEED (250)         // Minimum speed that can be timed by the hardware timer
 #define MIN_RAMP_TIME (100)     // Minimum ramp up/down time in milliseconds
 
@@ -23,15 +23,15 @@ constexpr int32_t MinStepPosition = -2000000000L;
 #define MOTOR_BOARD     (MOTOR_CONTROLLER_SHIELDMD10)
 #define ENCODER_PIN_A    (2)   // Encoder
 #define ENCODER_PIN_B    (3)   // Encoder
-#define MOTOR_MIN_PWM   (60)    // Minimum PWM setting needed to move the motor
+#define MOTOR_MIN_PWM   (100)    // Minimum PWM setting needed to move the motor
 // Acceleration PID constants
 const float DCMOTOR_kp_A = 0.00;
-const float DCMOTOR_ki_A = 0.2;
+const float DCMOTOR_ki_A = 0.1;
 const float DCMOTOR_kd_A = 0.00;
 // Running PID constants
-const float DCMOTOR_kp_R = 0.5;
+const float DCMOTOR_kp_R = 1.0;
 const float DCMOTOR_ki_R = 0.0;
-const float DCMOTOR_kd_R = 0.02;
+const float DCMOTOR_kd_R = 0.1;
 // BTS7960
 #define MOTOR_ENABLE_PIN_L  (7)
 #define MOTOR_ENABLE_PIN_R  (8)
@@ -55,7 +55,7 @@ const float DCMOTOR_kd_R = 0.02;
 #define ROTATOR_FULL_REVOLUTION_MICROSTEPS (42977)
 #define ROTATOR_MAX_POSITION (MaxStepPosition)
 #define ROTATOR_HOME_POSITION (0)
-#define ROTATOR_DEFAULT_DEADZONE_DEG (0.5)
+#define ROTATOR_DEFAULT_DEADZONE_DEG (0.1)
 #define ROTATOR_DEFAULT_DEADZONE (ROTATOR_FULL_REVOLUTION_MICROSTEPS / 360 * ROTATOR_DEFAULT_DEADZONE_DEG)	// default dead-zone in microsteps (~0.5°)
 
 // Serial Configuration
@@ -63,14 +63,14 @@ const float DCMOTOR_kd_R = 0.02;
 #define HOST_SERIAL_RX_BUFFER_SIZE (16) // Receive buffer for PC/USB communications
 
 // Other hardware assignments
-#define HOME_INDEX_PIN (13) //PIN13
-#define OPEN_LIMIT_SWITCH_PIN (12) //PIN3
-#define CLOSED_LIMIT_SWITCH_PIN (11) //PIN2
-#define OPEN_BUTTON_PIN (A1) //PIN5
-#define CLOSE_BUTTON_PIN (A2) //PIN6
+#define HOME_INDEX_PIN (18) //PIN13
+#define OPEN_LIMIT_SWITCH_PIN (18) //PIN3
+#define CLOSED_LIMIT_SWITCH_PIN (19) //PIN2
+#define OPEN_BUTTON_PIN (A6) //PIN5
+#define CLOSE_BUTTON_PIN (A7) //PIN6
 #define CLOCKWISE_BUTTON_PIN (OPEN_BUTTON_PIN)
 #define COUNTERCLOCKWISE_BUTTON_PIN (CLOSE_BUTTON_PIN)
-#define RAIN_SENSOR_PIN (A3) //PIN7
-#define BATTERY_SENSOR_PIN (A0)
+#define RAIN_SENSOR_PIN (A4) //PIN7
+#define BATTERY_SENSOR_PIN (A1)
 
 #endif // NEXDOME_H
